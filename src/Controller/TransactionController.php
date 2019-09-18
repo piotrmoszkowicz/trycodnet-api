c42801500ac375e2fc5c89b2a03a201e9946e66f
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 16:16:32 +0200

    Finish TransactionController

---
+++ src/Controller/TransactionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BlockchainTransaction;
+use App\Entity\BlockchainWallet;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+// TODO: Move getRepository to the service
+
+final class TransactionController extends AbstractFOSRestController
+{
+    /**
+     * Updates transaction details
+     * @Rest\Put("/transaction")
+     * @param Request $request
+     * @return View
+     * @throws \Exception
+     */
+    public function putTransaction(Request $request): View
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $wallet = $this->getDoctrine()->getRepository(BlockchainWallet::Class)->findOneBy(['address' => $request->get('trackedWalletId')]);
+        $transactions = $wallet->getBlockchainTransactions();
+
+        $transactionId = $request->get('transactionId');
+
+        $filteredTransactions = array_filter(
+            $transactions->toArray(),
+            function ($e) use ($transactionId) {
+                return $e->getTransactionId() == $transactionId;
+            }
+        );
+
+        $transaction = null;
+
+        if (count($filteredTransactions) > 0) {
+            $transaction = $filteredTransactions[0];
+            $transaction->setNumOfConfirmations($request->get('numberOfConfirmations'));
+
+        } else {
+            $date = new \DateTime();
+            $date->setTimestamp($request->get('date'));
+            $transaction = new BlockchainTransaction();
+            $transaction->setAddressTransaction($request->get('otherWalletId'));
+            $transaction->setAmount($request->get('amount'));
+            $transaction->setDate($date);
+            $transaction->setTransactionId($request->get('transactionId'));
+            $transaction->setNumOfConfirmations($request->get('numberOfConfirmations'));
+            $transaction->setTransactionType($request->get('transactionType'));
+            $wallet->addBlockchainTransaction($transaction);
+            $entityManager->persist($transaction);
+        }
+
+        $entityManager->flush();
+
+        return View::create($transaction, Response::HTTP_OK);
+    }
+}
